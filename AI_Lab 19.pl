9b7f431fe715663dfa10a96b7d9a11f24941cdf7
Nandivardhan Pola <119517401+nandunandivardhan@users.noreply.github.com>
2026-06-16 08:30:46 +0530

    Add files via upload

---
+++ AI_Lab 19.pl
@@ -0,0 +1,14 @@
+student(ram).
+student(sita).
+
+teacher(kumar).
+teacher(rao).
+
+subject(math).
+subject(science).
+
+teaches(kumar, math).
+teaches(rao, science).
+
+studies(ram, math).
+studies(sita, science).
